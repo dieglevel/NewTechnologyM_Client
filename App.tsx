ea75ad16ad6578d7ee9aebef0b26932be9cab2df
Phùng Anh Minh <dieglevel@gmail.com>
2024-12-31 11:52:37 +0700

    UPDATE: Font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { fonts } from './src/assets/fonts';
+import { Loading } from './src/apps/components';
 
 export default function App() {
+	const [fontsLoaded] = useFonts(fonts);
+
+	if (!fontsLoaded) {
+		return <Loading size={300} />;
+	} 
 
   return (
     <View>
-      <Text></Text>
+      <Text style={{fontFamily: ""}}></Text>
     </View>
   );
 }
