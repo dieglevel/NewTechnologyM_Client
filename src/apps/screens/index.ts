9d1a2765b2ca5f64a0ae595a79940cd8ba0691cc
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2025-01-19 20:03:13 +0700

    feat: Update screen exports and remove unused authentication components

--- src/apps/screens/index.ts
+++ src/apps/screens/index.ts
@@ -2,5 +2,5 @@ export * from "./home";
 export * from "./user/user";
 export * from "./register/register";
 export * from "./otp/otp"
-export * from "./login/login";
 export * from "./detail-user/detail-user"
+export * from "./login/login"
